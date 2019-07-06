dfdc9609139b83775b19c891755106bf77f8745e
Sakib Chowdhury <sm6chowd@manta.uwaterloo.ca>
2019-07-06 16:28:30 -0400

    Fix check for zsh

--- shell_setup.sh
+++ shell_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PATH_TO_ZSH=which zsh
+PATH_TO_ZSH=command -v zsh >/dev/null 2>&1
 
-if [ -x $PATH_TO_ZSH ]; then
+if [ ! -z $PATH_TO_ZSH ] && [ -x $PATH_TO_ZSH ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
