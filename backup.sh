05f32f96fc99859453c8c5aadbc940ba121f0a51
Sakib Chowdhury <hi.im.sakib@gmail.com>
2019-07-06 17:44:55 -0400

    Backup and overwrite bashrc if already exists

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+FILE_NAME=$1
+
+if [ ! -L $FILE_NAME ]; then
+  NEW_FILE_NAME=$FILE_NAME.backup
+  mv $FILE_NAME $NEW_FILE_NAME
+
+  echo "Backed up $FILE_NAME to $NEW_FILE_NAME"
+fi
