16e945d42269f9bcba48022fbe36ee12aebe4f04
Sakib Chowdhury <hi.im.sakib@gmail.com>
2019-07-06 17:35:17 -0400

    Add bash-it

--- shell_setup.sh
+++ shell_setup.sh
@@ -5,4 +5,8 @@ PATH_TO_ZSH=command -v zsh >/dev/null 2>&1
 if [ ! -z $PATH_TO_ZSH ] && [ -x $PATH_TO_ZSH ]; then
   # TODO: Check if ohmyzsh is already installed as well, to suppress the failure
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  # Set up bash-it instead
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+  $HOME/.bash_it/install.sh --no-modify-config
 fi
