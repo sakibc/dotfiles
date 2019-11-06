e7fc27736ad4d5b50e56d2140da5976fb2e78c91
Sakib Chowdhury <hi.im.sakib@gmail.com>
2019-11-06 15:14:43 -0500

    fixed again...

--- shell_setup.sh
+++ shell_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PATH_TO_ZSH=command -v zsh >/dev/null 2>&1
+PATH_TO_ZSH="$(command -v zsh)" 
 
 if [ ! -z $PATH_TO_ZSH ] && [ -x $PATH_TO_ZSH ]; then
   # TODO: Check if ohmyzsh is already installed as well, to suppress the failure
