db95d23064779faa8c5dcfbbcb6b0a5ab515dfcd
Sakib Chowdhury <sakib_c@outlook.com>
2020-08-20 15:44:34 -0400

    fix config issues...

--- shell_setup.sh
+++ shell_setup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-PATH_TO_ZSH="$(command -v zsh)" 
-
-if [ ! -z $PATH_TO_ZSH ] && [ -x $PATH_TO_ZSH ]; then
-  if [[ -d "$HOME/.oh-my-zsh" ]]; then
-    echo "Oh My Zsh is already installed."
-  else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  fi
+# Set up Oh My Zsh
+if [[ -d "$HOME/.oh-my-zsh" ]]; then
+  echo "Oh My Zsh is already installed."
 else
-  # Set up bash-it instead
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Set up Bash-it
+if [[ -d "$HOME/.bash_it" ]]; then
+  echo "Bash-it is already installed."
+else
   git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
   $HOME/.bash_it/install.sh --no-modify-config
 fi
--- vim_setup.sh
+++ vim_setup.sh
@@ -4,6 +4,7 @@ PATH_TO_VIM_PLUG=$HOME/.local/share/nvim/site/autoload/plug.vim
 
 # one-time download
 if [[ ! -f $PATH_TO_VIM_PLUG ]]; then
+  echo "Installing vim-plug..."
   curl -fLo $PATH_TO_VIM_PLUG --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
