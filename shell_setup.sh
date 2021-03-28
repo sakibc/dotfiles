68876e5d969c7b2dd8838f17944f8e65ec588003
Sakib Chowdhury <sakib_c@outlook.com>
2021-03-28 15:53:48 -0400

    fix some setup

--- shell_setup.sh
+++ shell_setup.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-# Set up Oh My Zsh
-if [[ -d "$HOME/.oh-my-zsh" ]]; then
-  echo "Oh My Zsh is already installed."
-else
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-# Set up Bash-it
-if [[ -d "$HOME/.bash_it" ]]; then
-  echo "Bash-it is already installed."
-else
-  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-  $HOME/.bash_it/install.sh --no-modify-config
-fi
+# Set up Antigen
+curl -L git.io/antigen > ~/.antigen.zsh
--- vim_setup.sh
+++ vim_setup.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
-PATH_TO_VIM_PLUG=$HOME/.local/share/nvim/site/autoload/plug.vim
-
-# one-time download
-if [[ ! -f $PATH_TO_VIM_PLUG ]]; then
-  echo "Installing vim-plug..."
-  curl -fLo $PATH_TO_VIM_PLUG --create-dirs \
+# Set up vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
 
 # idempotent
 nvim --headless +PlugInstall +qa
 
-# TODO: automatically run compilation
-# TODO: install nvim python requirement
-# TODO: install universal ctags
+# # TODO: automatically run compilation
+# # TODO: install nvim python requirement
+# # TODO: install universal ctags
 
-echo "YouCompleteMe has a compiled component. Please go to ~/.local/share/nvim/plugged/YouCompleteMe and run"
-echo "  python3 install.py --all"
-echo "to complete the installation."
+# echo "YouCompleteMe has a compiled component. Please go to ~/.local/share/nvim/plugged/YouCompleteMe and run"
+# echo "  python3 install.py --all"
+# echo "to complete the installation."
